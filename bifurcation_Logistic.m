2343cc9ef8384c6a60793337d5e56d804a2d4838
Masahisa Watanabe <s160479z@st.go.tuat.ac.jp>
2018-09-17 18:06:27 +0900

    Logistic_model
    May's logistci model is added.

---
+++ bifurcation_Logistic.m
@@ -0,0 +1,25 @@
+function bifurcation
+clear all;close all;
+
+a = 0;
+x(1) = 0.02;
+figure;
+for i =1:400
+    
+    for j = 1:1000
+        x(j+1) = a*x(j)*(1-x(j));        %logistic map  a=1:4
+        %x(j+1) =a*x(j)*(1+x(j))^(-6); %Hassell model a=0:60
+        %x(j+1) =a*x(j)*exp(-4*x(j));  %Ricker model  a=0:30
+        
+    end
+        
+        for k = 1:100
+            z(k) = x(1000+1-k);
+    hold on
+    plot(a,z);
+    a = a +0.01;
+end
+xlabel('a');
+ylabel('x');
+end
+    
